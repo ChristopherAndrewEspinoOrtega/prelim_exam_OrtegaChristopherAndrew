fd52e1a0d9180c02a09278e51c6f4c64a19c5364
ChristopherAndrewEspinoOrtega <excaliburodysyuss@yahoo.com>
2020-11-04 12:36:00 +0800

    Add project files.

---
+++ Models/Director.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class Director
+    {
+        public Director()
+        {
+            Movies = new HashSet<Movie>();
+
+        }
+
+        public Director(string MovieTitle)
+        {
+            Name = MovieTitle;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+
+    }
+}
